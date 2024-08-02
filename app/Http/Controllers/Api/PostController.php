d6540aba2cc54d16bfdf44e34ea763d6bd44d990
nancyk97 <112710318+nancyk97@users.noreply.github.com>
2024-08-02 15:54:33 +0530

    Update PostController.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {
         return response()->json([
-            'message' => 'Hello World'
+            'message' => 'Hello World1'
         ]);
     }
 }
